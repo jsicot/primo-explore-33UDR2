112220b4f75851664b7416f65609b35e5c22bf9e
Julien Sicot <julien.sicot@gmail.com>
2018-05-28 16:06:27 +0200

    merged custom angular modules

--- js/kohaItems.module.js
+++ js/kohaItems.module.js
@@ -1,31 +1,22 @@
-angular.module('kohaItems', []).component('prmFullViewServiceContainerAfter', {
+angular.module('kohaItems', []).component('prmOpacAfter', {
   bindings: { parentCtrl: '<' },
   controller: function controller($scope, $http, $element, kohaitemsService) {
     this.$onInit = function () {
       $scope.kohaDisplay = false; /* default hides template */
-	  var section = $scope.$parent.$ctrl.service.scrollId;
       var obj = $scope.$ctrl.parentCtrl.item.pnx.control;
+      var openurl;
       if (obj.hasOwnProperty("sourcerecordid") && obj.hasOwnProperty("sourceid")) {
         var bn = obj.sourcerecordid[0];
         var source = obj.sourceid[0];
         var type = $scope.$ctrl.parentCtrl.item.pnx.display.type[0];
-/*
-        console.log("source:" + bn);
-        console.log("biblionumber:" + bn);
-*/
-        if (bn && section == "getit_link1_0" && source == "33UDR2_KOHA" && type != "journal") {
+        if (bn && source == "33UDR2_KOHA" && type != "journal") {
           var url = "https://catalogue.bu.univ-rennes2.fr/r2microws/json.getSru.php?index=rec.id&q=" + bn;
           var response = kohaitemsService.getKohaData(url).then(function (response) {
-            console.log("it worked");
-            //console.log(response);
             var items = response.data.record[0].item;
-            console.log(items);
             var kohaid = response.data.record[0].biblionumber;
             var imagePath = response.data.record[0].cover;
-            console.log(kohaid);
             if (kohaid === null) {
               $scope.kohaDisplay = false;
-              console.log("it's false");
               $scope.kohaClass = "ng-hide";
             } else {
               $scope.kohaid = kohaid;
@@ -35,40 +26,63 @@ angular.module('kohaItems', []).component('prmFullViewServiceContainerAfter', {
               $scope.kohaClass = "ng-show";
             }
           });
-        } else {
-          $scope.kohaDisplay = false;
-        }
-      } else {
-        $scope.kohaClass = "ng-hide";
-      }
+        } else if (bn && source == "33UDR2_KOHA" && type == "journal") {
+	      	var url = "https://catalogue.bu.univ-rennes2.fr/r2microws/json.getSru.php?index=journals&q="+ bn;
+		  	var response = kohaitemsService.getKohaData(url).then(function (response) {
+				if (response.data.record != undefined && response.data.record.length > 0) {
+					console.log(response.data.record);
+					$scope.kohaholdings = [];
+					for (var i = 0 ; i < response.data.record[0].holdings.length ; i++) {
+						var holding = response.data.record[0].holdings[i]
+						$scope.kohaholdings[i] = {
+							"library" : holding["rcr"],
+							"holdings" : holding["holdings"]
+						};
+						if (holding["holdings"].length > 80) {
+							$scope.kohaholdings[i]["holdingsSummary"] = holding["holdings"].substring(0,77)+"...";
+						}
+						if (response.data.record[0].locations[i]["rcr"] ==  holding["rcr"]) {
+							$scope.kohaholdings[i]["callnumber"] =  response.data.record[0].locations[i]["callnumber"];
+							$scope.kohaholdings[i]["location"] =	response.data.record[0].locations[i]["location"];
+						}
+					}
+				}
+			});
+			this.onClick = function() {
+				 $window.open('https://catalogue.bu.univ-rennes2.fr/bib/'+ bn, '_blank');
+			};
+		} 
+		
+		var delivery = $scope.$ctrl.parentCtrl.item.delivery;
+		if (delivery != undefined) {
+			for (var i = 0 ; i < delivery.link.length ; i++){
+				if (delivery.link[i].displayLabel == "openurl") {
+					openurl = delivery.link[i].linkURL;
+				}
+			}
+		}
+		if (openurl != undefined){
+			openurl = openurl.replace(/.+\?/, "");
+			$scope.proxifiedurl = "https://cataloguepreprod.bu.univ-rennes2.fr/r2microws/getSfx.php?"+openurl;
+			$http.jsonp($scope.proxifiedurl).then(function(response) {
+				if (response.data.error == undefined) {
+					 var keys = Object.keys(response.data);
+					 var len = keys.length;
+					 console.log("SFX results: "+len);
+					 if(len > 0) {
+						  $scope.sfxholdings = response.data;
+					 }
+				}
+			}).catch(function(e) {
+				console.log(e);
+			});
+		}
+		
+		
+      } 
     };
   },
-  
-  template: `
- <!--<pre>{{items | json}}</pre>-->
-<div class="padding-left-medium">
-<md-list class="separate-list-items margin-bottom-medium padding-bottom-zero md-primoExplore-theme"" role="list">
-<md-list-item class="md-2-line _md-no-proxy _md" ng-repeat="item in items track by $index" role="listitem" ng-show="{{kohaDisplay}}" class="{{kohaClass}} role="listitem"">
-<div class="layout-full-width layout-display-flex md-ink-ripple layout-row" layout="flex">
-	<div layout="row" flex="100" layout-align="space-between center" class="layout-align-space-between-center layout-row flex-100">
-		<div class="md-list-item-text layout-wrap layout-row flex" layout="row" layout-wrap="" flex="">
-			<div flex="" flex-xs="100" class="flex-xs-100 flex">
-   			<h3>{{item.homebranch}}</h3>
-				<p>
-					<span ng-if="item.istatus" class="availability-status {{item.statusClass}}">{{item.istatus}}</span> 
-					<span>,</span> 
-					<span>{{item.location}}</span>
-					<span>;</span> 
-					<span>{{item.itemcallnumber}}</span>
-				</p>
-			</div>
-	</div>
-</div>
-</div>
-</md-list-item>
-</md-list>
-</div>
-  `
+  templateUrl: 'custom/33UDR2_VU1/html/prmOpacAfter.html'
 }).factory('kohaitemsService', ['$http', function ($http) {
   return {
     getKohaData: function getKohaData(url) {
---
+++ js/sfxHoldings.module.js
@@ -0,0 +1,39 @@
+angular.module('sfxHoldings', []).component('prmViewOnlineAfter', {
+  bindings: { parentCtrl: '<' },
+  controller: function controller($scope, $http, $element, sfxholdingsService) {
+    this.$onInit = function () {
+      var obj = $scope.$ctrl.parentCtrl.item.linkElement.links[0];
+      if (obj.hasOwnProperty("getItTabText") && obj.hasOwnProperty("displayText") && obj.hasOwnProperty("isLinktoOnline") && obj.hasOwnProperty("link")) {
+        if (obj['getItTabText'] == "tab1_full" && obj['isLinktoOnline'] == true && obj['displayText'] == "openurlfulltext") {
+	      console.log(obj);
+	      console.log(obj['link']);
+          var openurl = obj['link'];
+          var openurlSvc = openurl.replace("http://acceder.bu.univ-rennes2.fr/sfx_33puedb","https://cataloguepreprod.bu.univ-rennes2.fr/r2microws/getSfx.php");
+          var response = sfxholdingsService.getSfxData(openurlSvc).then(function (response) {
+            var holdings = response.data;
+            if (holdings === null) {
+	            
+            } else {
+	          console.log(holdings);
+              $scope.sfxholdings = holdings;
+              angular.element(document.querySelector('prm-view-online div a.arrow-link.md-primoExplore-theme'))[0].style.display = "none";
+            }
+          });
+        } 
+      } 
+    };
+  },
+  templateUrl: 'custom/33UDR2_VU1/html/prmViewOnlineAfter.html'
+}).factory('sfxholdingsService', ['$http', function ($http) {
+  return {
+    getSfxData: function getSfxData(url) {
+      return $http({
+        method: 'JSONP',
+        url: url
+      });
+    }
+  };
+}]).run(function ($http) {
+  // Necessary for requests to succeed...not sure why
+  $http.defaults.headers.common = { 'X-From-ExL-API-Gateway': undefined };
+});
