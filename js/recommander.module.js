544aff86764b0355011e268fb45d65f7a575730d
jsicot <julien.sicot@gmail.com>
2019-10-25 16:42:23 +0200

    show more results function for recommander resources

--- js/recommander.module.js
+++ js/recommander.module.js
@@ -8,7 +8,7 @@ angular.module('recommander', []).component('prmResourceRecommenderAfter', {
             parentCtrl: '<'
         },
         templateUrl: 'custom/' + viewName + '/html/recommander.html',
-        controller: ['$scope', '$rootScope', '$location', '$http', '$element', 'URLs', 'coursesService', 'recoService', function controller($scope, $rootScope, $location, $http, $element, URLs, coursesService, recoService) {
+        controller: ['$scope', '$rootScope', '$location', '$http', '$element', 'URLs', 'coursesService', 'recoService', '$mdDialog', function controller($scope, $rootScope, $location, $http, $element, URLs, coursesService, recoService, $mdDialog) {
             this.$onInit = function() {
                 var query = $location.search().query
                 var queries = Object.prototype.toString.call(query) === '[object Array]' ? query : query ? [query] : false
@@ -31,19 +31,21 @@ angular.module('recommander', []).component('prmResourceRecommenderAfter', {
                 }).catch(function(err) {
                     // handle errors here if needed
                 });
-                var query = queries.map(part => {
-                    let terms = part.split(',')
-                    let string = terms[2].normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[\s]+/g, " ") || ''
-                    return string
-                }).join(' ')
+                // var query = queries.map(part => {
+                //     let terms = part.split(',')
+                //     let string = terms[2].normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[\s]+/g, " ") || ''
+                //     return string
+                // }).join(' ')
+                var query = queries.map(part => part.split(",")[2] || "").join(' ');
                 console.log(query);
                 recoService.searchWwwbu(query).then(function(response) {
                     // no need to call user.data, service handles this
-                    if (response.response.docs) {
+                    if (response.response.docs && response.response.numFound > 0) {
+                        var numFound = response.response.numFound;
+                        $scope.numFound = numFound;
                         $element.parent().removeClass("ng-hide");
-                        $scope.recommandations = recoService.makeArray(response.response.docs)
-
-                        $scope.recommandations.map(
+                        var recommandations = recoService.makeArray(response.response.docs)
+                        recommandations.map(
                             resource => {
                                 resource.url = resource.ss_search_api_url
                                 if (resource.tm_field_link$url) {
@@ -54,21 +56,41 @@ angular.module('recommander', []).component('prmResourceRecommenderAfter', {
                                 }
                                 resource.image_url = resource.ss_field_image$file$url
                                 resource.name = resource.ss_title
+                                resource.label = resource.ss_title
                                 resource.description = resource.content
                             }
                         );
-
-                        console.log($scope.recommandations);
-
+                        $scope.recommandations = recommandations;
                     }
                 }).catch(function(err) {
                     // handle errors here if needed
                 });
+
+                function openOtherSuggests($event, recommandations) {
+                    $mdDialog.show({
+                        // template: templateString,
+                        templateUrl: 'custom/' + viewName + '/html/resource-recommender-dialog-template.html',
+                        targetEvent: $event,
+                        locals: {
+                            dialog: $mdDialog,
+                            //mediaQueries: mediaQueries,
+                            query: query,
+                            resources: recommandations
+                        },
+                        bindToController: true,
+                        clickOutsideToClose: true,
+                        fullscreen: true,
+                        escapeToClose: true,
+                        controllerAs: '$ctrl',
+                        controller: () => {}
+                    });
+                }
+                $scope.openOtherSuggests = openOtherSuggests;
             };
         }]
     })
-    .factory('recoService', ['$http', 'URLs',
-        function($http, URLs) {
+    .factory('recoService', ['$http', 'URLs', '$mdDialog',
+        function($http, URLs, $mdDialog) {
             return {
                 /**
                  * Converts a parameter to array if not already an array.
@@ -89,7 +111,7 @@ angular.module('recommander', []).component('prmResourceRecommenderAfter', {
                         url: URLs.wwwbu,
                         params: {
                             'q': `((index_id:databases or index_id:services or index_id:tools) and hash:ifv97h) and (${query})`,
-                            'limit': '3',
+                            'limit': '100',
                             'fl': 'ss_field_image$file$url,ss_title,ss_search_api_url,content,ss_field_resource_types,ss_field_access,sm_field_subjects$name,ss_type,bs_field_ezproxy,tm_field_link$url,ss_field_icon'
                         },
                         cache: true
--- js/sfxHoldings.module.js
+++ js/sfxHoldings.module.js
@@ -1,42 +1,43 @@
 import { viewName } from './viewName';
 
 angular.module('sfxHoldings', []).component('prmViewOnlineAfter', {
-  bindings: { parentCtrl: '<' },
+    bindings: { parentCtrl: '<' },
     controller: ['$scope', '$http', '$element', function controller($scope, $http, $element) {
-      this.$onInit = function () {
-      var obj = $scope.$ctrl.parentCtrl.item.linkElement.links[0];
-      if (obj.hasOwnProperty("getItTabText") && obj.hasOwnProperty("displayText") && obj.hasOwnProperty("isLinktoOnline") && obj.hasOwnProperty("link")) {
-        if (obj['displayText'] == "openurlfulltext") {
-	      $scope.sfxloading = true;
-	      console.log(obj);
-	      console.log(obj['link']);
-          var openurl = obj['link'];
-          var openurlSvc = openurl.replace("http://acceder.bu.univ-rennes2.fr/sfx_33puedb","https://catalogue.bu.univ-rennes2.fr/r2microws/getSfx.php");
-          $http({
-				method: 'JSONP',
-				url: openurlSvc,
-				headers: {
-					'Content-Type': 'application/json',
-					'X-From-ExL-API-Gateway': undefined
-					},
-					cache: true,
-			 }).then(function(response){
-            var holdings = response.data;
-            if (holdings === null) {
-	            
-            } else {
- 	          
- 	         if(angular.element(document.querySelector('prm-view-online div a.arrow-link.md-primoExplore-theme')).length > 0) {
-	 	         angular.element(document.querySelector('prm-view-online div a.arrow-link.md-primoExplore-theme'))[0].style.display = "none"; 
- 	         }
-	          $scope.sfxloading = false;
- 	          console.log(holdings);
-              $scope.sfxholdings = holdings;
+        this.$onInit = function() {
+            var obj = $scope.$ctrl.parentCtrl.item.linkElement.links[0];
+            console.log(obj);
+            if (obj.hasOwnProperty("getItTabText") && obj.hasOwnProperty("displayText") && obj.hasOwnProperty("isLinktoOnline") && obj.hasOwnProperty("link")) {
+                if (obj['displayText'] == "openurlfulltext") {
+                    $scope.sfxloading = true;
+                    console.log(obj);
+                    console.log(obj['link']);
+                    var openurl = obj['link'];
+                    var openurlSvc = openurl.replace("http://acceder.bu.univ-rennes2.fr/sfx_33puedb", "https://catalogue.bu.univ-rennes2.fr/r2microws/getSfx.php");
+                    $http({
+                        method: 'JSONP',
+                        url: openurlSvc,
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-From-ExL-API-Gateway': undefined
+                        },
+                        cache: true,
+                    }).then(function(response) {
+                        var holdings = response.data;
+                        if (holdings === null) {
+
+                        } else {
+
+                            if (angular.element(document.querySelector('prm-view-online div a.arrow-link.md-primoExplore-theme')).length > 0) {
+                                angular.element(document.querySelector('prm-view-online div a.arrow-link.md-primoExplore-theme'))[0].style.display = "none";
+                            }
+                            $scope.sfxloading = false;
+                            console.log(holdings);
+                            $scope.sfxholdings = holdings;
+                        }
+                    });
+                }
             }
-          });
-        } 
-      } 
-    };
-  }],
-  templateUrl: 'custom/'+viewName+'/html/prmViewOnlineAfter.html'
+        };
+    }],
+    templateUrl: 'custom/' + viewName + '/html/prmViewOnlineAfter.html'
 });
