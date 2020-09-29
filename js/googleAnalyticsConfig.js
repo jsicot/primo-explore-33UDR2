031a7e437569275784a94f0f7cb30c5bf8074f12
jsicot <julien.sicot@gmail.com>
2020-09-29 10:48:07 +0200

    removed GA module

--- js/googleAnalyticsConfig.js
+++
@@ -1,7 +0,0 @@
-export const googleAnalyticsConfig = Object.freeze({
-  name: 'googleAnalyticsConfig',
-  config: {
-    trackingId: "UA-4789419-7",
-    externalScriptURL: "https://www.googletagmanager.com/gtag/js?id=UA-4789419-7"
-  }
-});
